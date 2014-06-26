689df5c47b452665f679ac0b1b297692f3384e43
Georgiy <goshazvir@gmail.com>
2014-06-26 08:17:54 +0300

    upd index

--- js/common.js
+++ js/common.js
@@ -1,3 +1,32 @@
 head.ready(function() {
-	console.log("These aren't the droids you're looking for!");
+	// height
+	function height(){
+	  var getheight = $('.js-getHeight').height();
+	  $(".js-setHeight").css('min-height', getheight);
+  }
+  
+  if ($(".js-getHeight").length > 0){
+  	height();
+  }
+
+  //tab
+  function tab() {
+  	$(".js-tab").each(function(){
+  		var tab_link = $(this).find("a");
+  		var tab_item = $(this).find("li");
+  		var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+  		tab_cont.hide();
+  		tab_item.first().addClass("is-active");
+  		$(this).parents(".js-tab-group").find(".js-tab1").show();
+  		tab_link.on("click", function() {
+  			var index = $(this).attr("href");
+  			tab_item.removeClass("is-active");
+  			$(this).parent().addClass("is-active");
+  			tab_cont.hide();
+  			$(this).parents(".js-tab-group").find("."+index).show();
+  			return false;
+  		});
+  		});
+  } tab();
+
 });
